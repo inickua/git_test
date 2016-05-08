b0f81642ea890d79c6afa2edcc625cf597b2d1b3
inickua <inickua@ukr.net>
2016-05-08 23:08:11 +0300

    First commit

---
+++ js/core.js
@@ -0,0 +1,3 @@
+$(".model_gallary_imgs").colorbox({rel:'model_gallary_imgs'});
+$(".model_personal_img").colorbox({rel:'model_personal_img'});
+$(".service_gallary_imgs").colorbox({rel:'service_gallary_imgs'});
